91cff04db0b7ba666a1e54719ecc3aac0a4e6544
realth000 <a598919335@gmail.com>
2025-04-24 05:38:58 +0800

    revert: remove image, video and camera features

--- flutter_quill_extensions/lib/flutter_quill_extensions.dart
+++ flutter_quill_extensions/lib/flutter_quill_extensions.dart
@@ -1,21 +1,4 @@
 library;
 
 export 'src/common/extensions/controller_ext.dart';
-export 'src/editor/image/config/image_config.dart';
-export 'src/editor/image/config/image_web_config.dart';
-export 'src/editor/image/image_embed.dart';
-export 'src/editor/image/image_embed_types.dart';
-export 'src/editor/image/image_web_embed.dart';
-export 'src/editor/video/config/video_config.dart';
-export 'src/editor/video/config/video_web_config.dart';
-export 'src/editor/video/video_embed.dart';
-export 'src/editor/video/video_web_embed.dart';
 export 'src/flutter_quill_embeds.dart';
-export 'src/toolbar/camera/camera_button.dart';
-export 'src/toolbar/camera/camera_types.dart';
-export 'src/toolbar/camera/config/camera_config.dart';
-export 'src/toolbar/image/config/image_config.dart';
-export 'src/toolbar/image/image_button.dart';
-export 'src/toolbar/video/config/video.dart';
-export 'src/toolbar/video/config/video_config.dart';
-export 'src/toolbar/video/video_button.dart';
--- flutter_quill_extensions/lib/src/editor/video/config/video_web_config.dart
+++
@@ -1,6 +0,0 @@
-import 'package:meta/meta.dart' show immutable;
-
-@immutable
-class QuillEditorWebVideoEmbedConfig {
-  const QuillEditorWebVideoEmbedConfig();
-}
